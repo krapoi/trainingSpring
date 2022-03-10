6ecbb281dd109095c6a68e79d47b4cdb98ac3517
황지민 <80872558+krapoi@users.noreply.github.com>
2022-03-10 14:06:19 +0900

    Change HelloController.java, Add Member.java,MemberRepository.java, MemoryMemberRepository.java

--- src/main/java/com/train/trainingSpring/Controller/HelloController.java
+++ src/main/java/com/train/trainingSpring/Controller/HelloController.java
@@ -4,8 +4,8 @@ package com.train.trainingSpring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,6 +20,32 @@ public class HelloController {
     public String hellomvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hellotemplate";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String hellostring(@RequestParam("name") String name, Model model){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
     }
 
 }
---
+++ src/main/java/com/train/trainingSpring/Repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.train.trainingSpring.Repository;
+
+import com.train.trainingSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findbyId(Long id);
+    Optional<Member> findbyName(String name);
+    List<Member> findAll();
+}
