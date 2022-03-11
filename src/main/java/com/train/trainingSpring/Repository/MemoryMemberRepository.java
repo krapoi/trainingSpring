556b5b6c89b0c8a89181e9069d53082216b140ce
황지민 <80872558+krapoi@users.noreply.github.com>
2022-03-11 11:43:47 +0900

    Change MemberService.java, MemoryMemberRepository.java, Add MemberServiceTest.java, MemberController.java

--- src/main/java/com/train/trainingSpring/Repository/MemoryMemberRepository.java
+++ src/main/java/com/train/trainingSpring/Repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.train.trainingSpring.Repository;
 
 import com.train.trainingSpring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long,Member> store = new HashMap<>();
     private static long sequence = 0L;
--- src/main/java/com/train/trainingSpring/service/MemberService.java
+++ src/main/java/com/train/trainingSpring/service/MemberService.java
@@ -1,14 +1,21 @@
 package com.train.trainingSpring.service;
 
 import com.train.trainingSpring.Repository.MemberRepository;
-import com.train.trainingSpring.Repository.MemoryMemberRepository;
 import com.train.trainingSpring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원은 X
